43f616a2818f76b6eae50aa3f3aeadad3ee58624
PierreLeBlond <pierre.lespingal@gmail.com>
2025-07-03 22:58:17 +0200

    feat: Improve note collecting ux

--- scripts/collectibles/NoteCollector.gd
+++ scripts/collectibles/NoteCollector.gd
@@ -9,7 +9,16 @@ signal note_collected(count: int)
 func _ready() -> void:
     area_entered.connect(on_area_entered)
 
-func on_area_entered(collectible_area: Area2D) -> void:
-    collectible_area.queue_free()
+func on_area_entered(note: Note) -> void:
+    if (note is not Note):
+        return
+
+    note.set_collision_layer_value(4, false)
+
     collected_note_count += 1
     note_collected.emit(collected_note_count)
+
+    note.animation_player.play("fly")
+    await note.animation_player.animation_finished
+
+    note.queue_free()
--- scripts/ui/hud/CollectorLabel.gd
+++ scripts/ui/hud/CollectorLabel.gd
@@ -1,14 +1,18 @@
-extends Label
+extends Control
 
 class_name CollectorLabel
 
+@export var animation_player: AnimationPlayer
+@export var count_label: Label
 @export var collector: NoteCollector
 
 func _ready() -> void:
     collector.note_collected.connect(on_note_collected)
 
 func on_note_collected(count: int) -> void:
+    animation_player.play("pulse")
+
     if count < 10:
-        text = "0" + str(count)
+        count_label.text = "0" + str(count)
     else:
-        text = str(count)
+        count_label.text = str(count)
