57ee1438923dfd94dd17a5c6b7e2ae6966f5fbdb
PierreLeBlond <pierre.lespingal@gmail.com>
2025-07-16 12:29:43 +0200

    fix: Fix clear color blink between scene

--- scripts/ui/TitleScreen.gd
+++ scripts/ui/TitleScreen.gd
@@ -3,16 +3,16 @@ extends Control
 class_name TitleScreen
 
 @export var tutorial_button: Button
-@export var escape_level_button: Button
+@export var level_1_button: Button
 
 func _ready() -> void:
     tutorial_button.pressed.connect(on_tutorial_button_pressed)
-    escape_level_button.pressed.connect(on_escape_level_button_pressed)
+    level_1_button.pressed.connect(on_level_1_button_pressed)
 
 func on_tutorial_button_pressed() -> void:
     var tree = get_tree()
     tree.change_scene_to_file("res://scenes/levels/Tutorial.tscn")
 
-func on_escape_level_button_pressed() -> void:
+func on_level_1_button_pressed() -> void:
     var tree = get_tree()
-    tree.change_scene_to_file("res://scenes/levels/Escape.tscn")
+    tree.change_scene_to_file("res://scenes/levels/Level1.tscn")
