2cf0ef02fac21ad1cc44f97f050adea8e2299063
PierreLeBlond <pierre.lespingal@gmail.com>
2025-07-18 13:28:37 +0200

    fix: Fix wall run at peak of wall

--- scripts/character/states/WallRun.gd
+++ scripts/character/states/WallRun.gd
@@ -14,13 +14,19 @@ class_name WallRun
 
 var jump_pressed_time: float = 0
 
+var wall_friction_factor: float = 1.0
+
 func enter(previous_state: State, delta: float) -> void:
     super (previous_state, delta)
 
+    wall_friction_factor = parent.projectile_parameters.wall_friction_factor
     jump_pressed_time = 0
 
 func update(delta: float) -> void:
     jump_pressed_time += delta
+
+    if (!is_on_wall()):
+        wall_friction_factor = 1.0
 
 func get_next_state(_delta: float) -> State:
     if (parent.is_in_gravity_field):
@@ -35,14 +41,14 @@ func get_next_state(_delta: float) -> State:
     if (parent.velocity.y > 0):
         return fall
 
-    if (!is_on_wall() || (parent.cancel_jump() && jump_pressed_time > parent.projectile_parameters.minimum_jump_pressed_time)):
+    if (parent.cancel_jump() && jump_pressed_time > parent.projectile_parameters.minimum_jump_pressed_time):
         return canceled_jump
     
     return null
 
 func get_parameters() -> Dictionary:
     return {
-        "jump_height": parent.projectile_parameters.jump_height * parent.projectile_parameters.wall_friction_factor,
+        "jump_height": parent.projectile_parameters.jump_height * wall_friction_factor,
         "jump_time": parent.projectile_parameters.jump_time,
         "maximum_lateral_velocity": maximum_lateral_velocity,
         "acceleration_factor": parent.projectile_parameters.acceleration_factor,
