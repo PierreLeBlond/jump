29d338a14bb65251b119de08ee7ae8ba3493b9bb
PierreLeBlond <pierre.lespingal@gmail.com>
2025-10-19 11:26:36 +0200

    feat: Add white flash on note captured

--- scripts/collectibles/Note.gd
+++ scripts/collectibles/Note.gd
@@ -6,6 +6,9 @@ const DEFAULT_BPM: float = 60.0
 const note_layer = 4
 
 @export var bpm: float = DEFAULT_BPM
+@export var flashed_sprite: ColorRect
+
+
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func _ready() -> void:
@@ -25,3 +28,9 @@ func capture():
     animation_player.animation_finished.disconnect(play_bounce)
     set_collision_layer_value(note_layer, false)
     animation_player.play("fly")
+    flashed_sprite.show()
+
+    var tween = create_tween()
+    tween.tween_property(flashed_sprite, "modulate:a", 0.0, 0.5).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN)
+    await tween.finished
+    flashed_sprite.hide()
